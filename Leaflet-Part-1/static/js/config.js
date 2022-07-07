b383cc772c8d4b871cd6f06a99111f728067b829
wwright21 <williamcwrightjr@gmail.com>
2022-07-07 00:34:54 -0400

    completed first portion of homework

--- Leaflet-Part-1/static/js/config.js
+++
@@ -1,1 +0,0 @@
-var API_KEY = "pk.eyJ1IjoianVhOTEiLCJhIjoiY2tjaDRuNTk4MHQ5YjMybzNjYnB3cWN3cCJ9.gVlF_ReTkACUGOm96BNssQ";
--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,66 +1,131 @@
 // Store our API endpoint as queryUrl.
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
+// create a map object
+var myMap = L.map("map", {
+  center: [
+    37.09, -95.71
+  ],
+  zoom: 5
+});
+
+// add a tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+
+// Function to determine circle color based on the depth in km 
+function getColor(depth){
+  switch(true){
+      case (depth <= 1):
+          return '#f2f0f7';
+          break;
+      case (depth <= 2):
+          return '#dadaeb';
+          break;
+      case (depth <= 3):
+          return '#bcbddc';
+          break;
+      case (depth <= 4):
+          return '#9e9ac8';
+          break;
+      case (depth <= 5):
+          return '#756bb1';
+          break;
+      case (depth > 5):
+          return '#54278f';
+          break;
+      default:
+          return '#cccccc';
+          break;
+  }
+}
+
+// Function to determine circle radius based on the magnitude 
+function getRadius(magnitude){
+  switch(true){
+      case (magnitude <= 1):
+          return 3;
+          break;
+      case (magnitude <= 2):
+          return 7;
+          break;
+      case (magnitude <= 3):
+          return 11;
+          break;
+      case (magnitude <= 4):
+          return 15;
+          break;
+      case (magnitude <= 5):
+          return 21;
+          break;
+      case (magnitude > 5):
+          return 28;
+          break;
+      default:
+          return 1;
+          break;
+  }
+}  
+
+
 // Perform a GET request to the query URL/
 d3.json(queryUrl).then(function (data) {
-    // Once we get a response, send the data.features object to the createFeatures function.
-    createFeatures(data.features);
-  });
-
-  function createFeatures(earthquakeData) {
-
-    // Define a function that we want to run once for each feature in the features array.
-    // Give each feature a popup that describes the place and time of the earthquake.
-    function onEachFeature(feature, layer) {
-      layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+  // Once we get a response, send the data.features object to the createFeatures function.
+  
+  L.geoJson(data,{
+    pointToLayer: function (feature, latlng) {
+        // Create a circle marker
+        return L.circleMarker(latlng, {
+            radius: getRadius(feature.properties.mag), // different radius for different magnitude
+            fillColor: getColor(feature.geometry.coordinates[2]), // different circle colors for different depths
+            color: "#000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8
+        });
+    },
+    onEachFeature: function(feature, layer){
+        layer.bindPopup(`<span>Earthquake magnitude: ${feature.properties.mag}</span><hr><span>Depth (km): ${feature.geometry.coordinates[2]}</span>`)
     }
-  
-    // Create a GeoJSON layer that contains the features array on the earthquakeData object.
-    // Run the onEachFeature function once for each piece of data in the array.
-    var earthquakes = L.geoJSON(earthquakeData, {
-      onEachFeature: onEachFeature
-    });
-  
-    // Send our earthquakes layer to the createMap function/
-    createMap(earthquakes);
-  }
+}).addTo(myMap);
 
-  function createMap(earthquakes) {
+// Create a legend
 
-    // Create the base layers.
-    var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    })
-  
-    var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-      attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-    });
-  
-    // Create a baseMaps object.
-    var baseMaps = {
-      "Street Map": street,
-      "Topographic Map": topo
-    };
-  
-    // Create an overlay object to hold our overlay.
-    var overlayMaps = {
-      Earthquakes: earthquakes
-    };
-  
-    // Create our map, giving it the streetmap and earthquakes layers to display on load.
-    var myMap = L.map("map", {
-      center: [
-        37.09, -95.71
-      ],
-      zoom: 5,
-      layers: [street, earthquakes]
-    });
-  
-    // Create a layer control.
-    // Pass it our baseMaps and overlayMaps.
-    // Add the layer control to the map.
-    L.control.layers(baseMaps, overlayMaps, {
-      collapsed: false
-    }).addTo(myMap);
-  
-  }
+var legend = L.control({ position: "bottomleft" });
+
+legend.onAdd = function(myMap) {
+  var div = L.DomUtil.create("div", "legend");
+  div.innerHTML += "<h4>Earthquake Depth</h4>";
+  div.innerHTML += '<i style="background: #f2f0f7"></i><span> Less than 1 km</span><br>';
+  div.innerHTML += '<i style="background: #dadaeb"></i><span> Less than 2 km</span><br>';
+  div.innerHTML += '<i style="background: #E6E696"></i><span> Less than 3 km</span><br>';
+  div.innerHTML += '<i style="background: #E8E6E0"></i><span> Less than 4 km</span><br>';
+  div.innerHTML += '<i style="background: #FFFFFF"></i><span> Less than 5 km</span><br>';
+
+  return div;
+};
+
+legend.addTo(myMap);
+
+
+// // other way to create a legend
+// var legend = L.control({position: 'bottomright'});
+// legend.onAdd = function (map) {
+
+//     var div = L.DomUtil.create('div', 'info legend'),
+//         mag = [0, 1, 2, 3, 4, 5]
+    
+//     div.innerHTML += "<h4>Magnitude Level</h4><hr>"
+//     // loop through our density intervals and generate a label with a colored square for each interval
+//     for (var i = 0; i < mag.length; i++) {
+//         div.innerHTML +=
+//             '<i style="background:' + getColor(mag[i] + 1) + '"></i> ' +
+//             mag[i] + (mag[i + 1] ? '&ndash;' + mag[i + 1] + '<br>' : '+');
+//     }
+//     return div;
+// };
+// legend.addTo(myMap);
+
+})
